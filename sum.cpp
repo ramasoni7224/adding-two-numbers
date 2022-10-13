729eb3b0a8c5b36039fec4d3c21095a0debd23a9
ramasoni7224 <98272091+ramasoni7224@users.noreply.github.com>
2022-10-13 11:12:40 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+  cout<<"Hello Viewers"<<endl;
+  cout<<"Enter First no.=";
+  cin>>a;
+  
+  cout<<"Enter Second no.=";
+  cin>>b;
+  
+  cout<<"Sum"<<a+b;
+  return 0;
+}
